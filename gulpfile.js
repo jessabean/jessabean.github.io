fb5a640bf48dda5093364d193e4bbe7b941ec695
jessabean <jessica.avison@gmail.com>
2017-07-23 16:26:03 -0400

    Home page content

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync');
-var prefix      = require('gulp-autoprefixer');
-var sass        = require('gulp-sass');
-var cp          = require('child_process');
-var ghPages     = require('gulp-gh-pages');
+var gulp        = require('gulp'),
+    browserSync = require('browser-sync'),
+    prefix      = require('gulp-autoprefixer'),
+    sass        = require('gulp-sass'),
+    cp          = require('child_process'),
+    ghPages     = require('gulp-gh-pages');
 
 var jekyll   = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
 var messages = {
@@ -35,9 +35,8 @@ gulp.task('sass', function () {
             onError: browserSync.notify
         }))
         .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-        .pipe(gulp.dest('_site/css'))
-        .pipe(browserSync.reload({stream:true}))
-        .pipe(gulp.dest('css'));
+        .pipe(gulp.dest('css'))
+        .pipe(browserSync.reload({stream:true}));
 });
 
 gulp.task('watch', function () {
